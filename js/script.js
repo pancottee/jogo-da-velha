43522967b47039c33babc5a60c42891a6dc0dcc5
pancotte <126676608+pancottee@users.noreply.github.com>
2023-05-24 16:54:13 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-alert("Ola mundo");
+let btn1 = 0;
+let btn2 = 0;
+let btn3 = 0;
+let btn4 = 0;
+let btn5 = 0;
+let btn6 = 0;
+let btn7 = 0;
+let btn8 = 0;
+let btn9 = 0;
+let control = 0;
+let jogadas = 0;
+let finaljogo = false;
+
+function clickBtn1() {
+    let btn = document.getElementById('btn1');
+
+if (btn.value == " " && !finaljogo) {
+    if (controle == 0) {
+        controle == 1;
+        btn.value = "X"
+        btn1 = "X"
+    } 
+    else {
+        controle = 0;
+        btn.value = "0"
+        btn1 = "O"
+    }
+}
+}
